06fc8169a85f8f1e555c2ba87cf59ab30b49fb43
konstantin <konstantin.klein@hochfrequenz.de>
2024-01-31 13:11:41 +0000

    Add first Unit and Integration Tests

---
+++ TransformerBeeClient/TransformerBeeClient.UnitTest/ConnectionTests.cs
@@ -0,0 +1,21 @@
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace TransformerBeeClient.UnitTest;
+
+public class ConnectionTests
+{
+    [Fact]
+    public void IsAvailable_Throws_ArgumentNullException_If_BaseAddress_Is_Not_Configured()
+    {
+        var services = new ServiceCollection();
+        services.AddHttpClient("TransformerBee", client =>
+        {
+            client.BaseAddress = null;
+        });
+        var serviceProvider = services.BuildServiceProvider();
+        var instantiateClient = () => new TransformerBeeRestClient(serviceProvider.GetService<IHttpClientFactory>());
+        instantiateClient.Should().Throw<ArgumentNullException>();
+    }
+}
---
+++ TransformerBeeClient/TransformerBeeClient/RestClient.cs
@@ -0,0 +1,46 @@
+﻿namespace TransformerBeeClient;
+
+/// <summary>
+/// a client for the transformer.bee REST API
+/// </summary>
+public class TransformerBeeRestClient
+{
+    private readonly HttpClient _httpClient;
+
+    /// <summary>
+    /// Provide the constructor with a http client factory.
+    /// It will create a client from said factory and use the <paramref name="clientName"/> for that.
+    /// </summary>
+    /// <param name="httpClientFactory">factory to create the http client from</param>
+    /// <param name="clientName">name used to create the client</param>
+    public TransformerBeeRestClient(IHttpClientFactory httpClientFactory, string clientName = "TransformerBee")
+    {
+        _httpClient = httpClientFactory.CreateClient(clientName);
+        if (_httpClient.BaseAddress == null)
+        {
+            throw new ArgumentNullException(nameof(httpClientFactory), $"The http client factory must provide a base address for the client with name '{clientName}'");
+        }
+    }
+
+    /// <summary>
+    /// tests if transformer bee is available
+    /// </summary>
+    /// <remarks>
+    /// Note that this does _not_ check if you're authenticated.
+    /// The method will probably throw an <see cref="HttpRequestException"/> if the host cannot be found.
+    /// </remarks>
+    /// <returns>
+    /// Returns true iff the transformer bee is available under the configured base address.
+    /// </returns>
+    public async Task<bool> IsAvailable()
+    {
+        var uriBuilder = new UriBuilder(_httpClient!.BaseAddress)
+        {
+            Path = "/version"
+        };
+
+        var versionUrl = uriBuilder.Uri.AbsoluteUri;
+        var response = await _httpClient.GetAsync(versionUrl);
+        return response.IsSuccessStatusCode;
+    }
+}
