417c28e7e2f6e11e70a1a8e390a22f3c27507197
konstantin <konstantin.klein@hochfrequenz.de>
2024-02-01 20:03:14 +0000

    try?

--- TransformerBeeClient/TransformerBeeClient.IntegrationTest/EdifactToBo4eTests.cs
+++ TransformerBeeClient/TransformerBeeClient.IntegrationTest/EdifactToBo4eTests.cs
@@ -31,3 +31,34 @@ public class EdifactToBo4eTests : IClassFixture<ClientFixture>
         result.Single().Transaktionen.Should().NotBeNullOrEmpty();
     }
 }
+
+/// <summary>
+/// those tests only run if the environment variables are set up correctly
+/// </summary>
+public class EdifactToBo4eTestsWithAuthentication : IClassFixture<GithubActionClientFixture>
+{
+    private readonly GithubActionClientFixture _client;
+
+    private readonly ITransformerBeeAuthenticationProvider? _authenticationProvider;
+
+    public EdifactToBo4eTestsWithAuthentication(GithubActionClientFixture clientFixture)
+    {
+        _client = clientFixture;
+        _authenticationProvider = clientFixture.AuthenticationProvider;
+    }
+
+    [Fact]
+    public async Task Edifact_Can_Be_Converted_To_Bo4e_With_Authentication()
+    {
+        if (_authenticationProvider is null)
+        {
+            return; // skip the test if no authentication provider is available
+        }
+        var httpClientFactory = _client.HttpClientFactory;
+        ICanConvertToBo4e client = new TransformerBeeRestClient(httpClientFactory, _authenticationProvider);
+        var edifactString = await File.ReadAllTextAsync("TestEdifacts/FV2310/55001.edi");
+        var result = await client.ConvertToBo4e(edifactString, EdifactFormatVersion.FV2310);
+        result.Should().BeOfType<List<Marktnachricht>>();
+        result.Single().Transaktionen.Should().NotBeNullOrEmpty();
+    }
+}
---
+++ TransformerBeeClient/TransformerBeeClient.IntegrationTest/GithubActionClientFixture.cs
@@ -0,0 +1,34 @@
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace TransformerBeeClient.IntegrationTest;
+
+/// <summary>
+/// A fixture that sets up the http client factory and an injectable service collection.
+/// It's thought to be used in the Github action.
+/// </summary>
+public class GithubActionClientFixture : IClassFixture<GithubActionClientFixture>
+{
+    public readonly IHttpClientFactory? HttpClientFactory;
+
+    public readonly ServiceCollection? ServiceCollection;
+
+    public readonly ITransformerBeeAuthenticationProvider? AuthenticationProvider;
+
+    public GithubActionClientFixture()
+    {
+        var clientId = Environment.GetEnvironmentVariable("CLIENT_ID");
+        var clientSecret = Environment.GetEnvironmentVariable("CLIENT_SECRET");
+        if (string.IsNullOrWhiteSpace(clientId) || string.IsNullOrWhiteSpace(clientSecret))
+        {
+            return;
+        }
+
+        var services = new ServiceCollection();
+        services.AddHttpClient("TransformerBee", client => { client.BaseAddress = new Uri("http://transformerstage.utilibee.io"); });
+        var serviceProvider = services.BuildServiceProvider();
+        ServiceCollection = services;
+        HttpClientFactory = serviceProvider.GetService<IHttpClientFactory>();
+        AuthenticationProvider = new ClientIdClientSecretAuthenticationProvider(clientId: clientId, clientSecret: clientSecret);
+    }
+}
